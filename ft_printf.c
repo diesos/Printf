fc1ca1f07870dea719e4068f62d6bba5a86814a6
Diesos <ozturk@live.Fr>
2024-04-29 23:31:40 +0200

    Add ft_printf.c file

---
+++ ft_printf.c
@@ -0,0 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omeoztur <omeoztur@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/29 23:23:56 by omeoztur          #+#    #+#             */
+/*   Updated: 2024/04/29 23:24:50 by omeoztur         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_printf(const char *format, ...)
+
+	// TODO: printf return an integer, counting all char to be printed.
+	// also using variadic.
