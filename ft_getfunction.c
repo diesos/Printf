85f27bca1a86731d97eee0f593d181b8b1a70ba3
Diesos <ozturk@live.Fr>
2024-04-29 23:31:34 +0200

    Add ft_getfunction.c file

---
+++ ft_getfunction.c
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_getfunction.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omeoztur <omeoztur@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/29 23:22:26 by omeoztur          #+#    #+#             */
+/*   Updated: 2024/04/29 23:23:52 by omeoztur         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int (*ft_getfunction(const char *nextChar))(va_list)
+{
+	// TODO: create a structure for handling, detecting char and poiting to
+	//      respective function.
+}
